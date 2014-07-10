a1e4aabe8702426755c6b08b07ac2d9a6e3a1763
rsb4114 <rsb4114@gmail.com>
2014-07-11 02:31:55 +0530

    July 11

---
+++ plot2.R
@@ -0,0 +1,47 @@
+##ED Assignment1
+
+#Individual household electric power consumption Data Set
+#The following descriptions of the 9 variables in the dataset :
+#Date: Date in format dd/mm/yyyy
+#Time: time in format hh:mm:ss
+#Global_active_power: household global minute-averaged active power (in kilowatt)
+#Global_reactive_power: household global minute-averaged reactive power (in kilowatt)
+#Voltage: minute-averaged voltage (in volt)
+#Global_intensity: household global minute-averaged current intensity (in ampere)
+#Sub_metering_1: energy sub-metering No. 1 (in watt-hour of active energy). It corresponds to the kitchen, containing mainly a dishwasher, an oven and a microwave (hot plates are not electric but gas powered).
+#Sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It corresponds to the laundry room, containing a washing-machine, a tumble-drier, a refrigerator and a light.
+#Sub_metering_3: energy sub-metering No. 3 (in watt-hour of active energy). It corresponds to an electric water-heater and an air-conditioner.
+
+library(data.table)
+
+initial <- read.table("./household_power_consumption.txt", header = TRUE, sep = ";", nrows = 100) 
+
+classes <- sapply(initial, class)
+cnames <- colnames(initial)
+
+#read relevant data
+hhep <- read.table("./household_power_consumption.txt", header = FALSE, sep = ";", colClasses = classes, col.names = cnames, skip = 66000, nrows = 4000)
+
+#Format Date Time
+hhep$DateTime <- paste (hhep$Date, hhep$Time)
+hhep$DateTime <- strptime(hhep$DateTime, "%d/%m/%Y %H:%M:%S")
+
+#convert to date format
+hhep$Date <- as.Date(hhep$Date, format = "%d/%m/%Y")
+class(hhep$Date)
+
+
+#filter only dates between 2007-02-01 and 2007-02-01
+reqdaterows <- (hhep$Date >= "2007-02-01") & (hhep$Date <= "2007-02-02")
+sum(reqdaterows)
+hhep <- hhep[reqdaterows, ]
+
+#Construct the plot and save it to a PNG file 
+#with a width of 480 pixels and a height of 480 pixels
+
+#Plot 2
+#Copy the plot to a PNG file
+png (file = "plot2.png", width = 480, height = 480)
+#plot of Global active power vs DateTime
+with (hhep, plot(DateTime, Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = ""))
+dev.off() ## close the PNG device
---
+++ plot3.R
@@ -0,0 +1,51 @@
+##ED Assignment1
+
+#Individual household electric power consumption Data Set
+#The following descriptions of the 9 variables in the dataset :
+#Date: Date in format dd/mm/yyyy
+#Time: time in format hh:mm:ss
+#Global_active_power: household global minute-averaged active power (in kilowatt)
+#Global_reactive_power: household global minute-averaged reactive power (in kilowatt)
+#Voltage: minute-averaged voltage (in volt)
+#Global_intensity: household global minute-averaged current intensity (in ampere)
+#Sub_metering_1: energy sub-metering No. 1 (in watt-hour of active energy). It corresponds to the kitchen, containing mainly a dishwasher, an oven and a microwave (hot plates are not electric but gas powered).
+#Sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It corresponds to the laundry room, containing a washing-machine, a tumble-drier, a refrigerator and a light.
+#Sub_metering_3: energy sub-metering No. 3 (in watt-hour of active energy). It corresponds to an electric water-heater and an air-conditioner.
+
+library(data.table)
+
+initial <- read.table("./household_power_consumption.txt", header = TRUE, sep = ";", nrows = 100) 
+
+classes <- sapply(initial, class)
+cnames <- colnames(initial)
+
+#read relevant data
+hhep <- read.table("./household_power_consumption.txt", header = FALSE, sep = ";", colClasses = classes, col.names = cnames, skip = 66000, nrows = 4000)
+
+#Format Date Time
+hhep$DateTime <- paste (hhep$Date, hhep$Time)
+hhep$DateTime <- strptime(hhep$DateTime, "%d/%m/%Y %H:%M:%S")
+
+#convert to date format
+hhep$Date <- as.Date(hhep$Date, format = "%d/%m/%Y")
+class(hhep$Date)
+
+
+#filter only dates between 2007-02-01 and 2007-02-01
+reqdaterows <- (hhep$Date >= "2007-02-01") & (hhep$Date <= "2007-02-02")
+sum(reqdaterows)
+hhep <- hhep[reqdaterows, ]
+
+#Construct the plot and save it to a PNG file 
+#with a width of 480 pixels and a height of 480 pixels
+
+#Plot3
+#Copy the plot to a PNG file
+png (file = "plot3.png", width = 480, height = 480)
+with(hhep, plot(DateTime, Sub_metering_1, type = "n", ylab = "Energy sub metering", xlab = ""))
+with(hhep, lines(DateTime, Sub_metering_1, col = "black"))
+with(hhep, lines(DateTime, Sub_metering_2, col = "red"))
+with(hhep, lines(DateTime, Sub_metering_3, col = "blue"))
+legend("topright", lwd = 2, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off() ## close the PNG device
+
